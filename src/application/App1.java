e59f39328e76f36c963b0f4eba917698dcca9c24
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-10-21 23:47:06 -0300

    Exemplo de configuração jdbc

--- src/application/App1.java
+++ src/application/App1.java
@@ -1,8 +1,15 @@
 package application;
 
+import java.sql.Connection;
+
+import db.DB;
+
 public class App1 {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
 
 	}
 
